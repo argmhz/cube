9264e09c64f0cb0f81883777be6e79d4194f0868
Marcus Daniel Hertz <arg_marcus@yahoo.dk>
2021-06-16 21:37:35 +0100

    created a generator tool

---
+++ generate.sh
@@ -0,0 +1,35 @@
+
+if [ $# -eq 0 ] ; then
+    echo 'No name given'
+    exit 0
+fi
+
+FILE=./animations/$1.cpp
+if [ -f "$FILE" ]; then
+    echo "$FILE already exists."
+    exit 0
+fi
+
+echo "generating $1"
+
+echo '
+#include "../lib/Cube.cpp"
+#include "../lib/Animation.hpp"
+#include "../lib/helpers.h"
+
+class '$1' : public Animation {
+
+  void draw(Cube *cube) {
+
+
+  }
+
+};
+extern "C" Animation * create() {
+    return new '$1';
+}
+
+extern "C" void destroy(Animation * p) {
+    delete p;
+}
+' >> ./animations/$1.cpp
